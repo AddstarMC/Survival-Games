b453c518859b307db343f2b3239f35058cc91d39
addstar <adam@addstar.com.au>
2013-10-31 23:28:39 +1100

    Clean up chest selection code

--- src/main/java/org/mcsg/survivalgames/events/ChestReplaceEvent.java
+++ src/main/java/org/mcsg/survivalgames/events/ChestReplaceEvent.java
@@ -27,33 +27,55 @@ public class ChestReplaceEvent implements Listener{
 	private Random rand = new Random();
 	
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled=true)
-    public void ChestListener(PlayerInteractEvent e){
-    	if(e.getAction() == Action.RIGHT_CLICK_BLOCK){
+    public void ChestListener(PlayerInteractEvent e) {
+    	if (e.getAction() == Action.RIGHT_CLICK_BLOCK) {
     		BlockState clicked = e.getClickedBlock().getState();
-    		if(clicked instanceof Chest || clicked instanceof DoubleChest){
+    		if (clicked instanceof Chest || clicked instanceof DoubleChest) {
+    			// Chest has been right clicked
     			int gameid = GameManager.getInstance().getPlayerGameId(e.getPlayer());
-    			if(gameid != -1){
+    			if (gameid != -1) {
     				Game game = GameManager.getInstance().getGame(gameid);
-    				if(game.getMode() == GameMode.INGAME){
+    				if (game.getMode() == GameMode.INGAME) {
+    					// Grab the "opened chest" list or create an empty one
     					HashSet<Block>openedChest = GameManager.openedChest.get(gameid);
-    					openedChest = (openedChest == null)? new HashSet<Block>() : openedChest;
-    					if(!openedChest.contains(e.getClickedBlock())){
-    						Inventory[] invs = ((clicked instanceof Chest))? new Inventory[] {((Chest) clicked).getBlockInventory()}
-    						: new Inventory[] {((DoubleChest)clicked).getLeftSide().getInventory(), ((DoubleChest)clicked).getRightSide().getInventory()};
+    					if (openedChest == null) {
+    						openedChest = new HashSet<Block>();
+    					}
 
-    						for(Inventory inv : invs){
+    					// If chest hasn't been opened yet.. we have to fill it!
+    					if (!openedChest.contains(e.getClickedBlock())) {
+    					
+    						// Single or double chest?
+    						Inventory[] invs = null;
+    						if (clicked instanceof Chest) {
+    							invs = new Inventory[] {
+    									((Chest) clicked).getBlockInventory()
+    							};
+    						} else {
+    							invs = new Inventory[] {
+    									((DoubleChest) clicked).getLeftSide().getInventory(),
+    									((DoubleChest) clicked).getRightSide().getInventory()
+    							};
+    						}
+
+    						// Loop through all the item lists, pick some items and randomly scatter them
+    						for (Inventory inv : invs) {
     							inv.setContents(new ItemStack[inv.getContents().length]);
     							
     							List<ItemStack> chestContents = ChestRatioStorage.getInstance().getItems();
     							
-    				            for(ItemStack i : chestContents){
+    				            for (ItemStack i : chestContents) {
+    				                // Find a random empty slot
     				                int l = rand.nextInt(26);
-    				                while(inv.getItem(l) != null)
+    				                while(inv.getItem(l) != null) {
     				                    l = rand.nextInt(26);
-    				                inv.setItem(l, i);
+    				                }
+    				                inv.setItem(l, i); 		// Add selected item to this chest
     				            }
     						}
     					}
+    					
+    					// 
     					openedChest.add(e.getClickedBlock());
     					GameManager.openedChest.put(gameid, openedChest);
     				} else {
