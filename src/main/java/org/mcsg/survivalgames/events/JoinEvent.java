e90011cee81e855313f1158b9328a91908534d90
addstar <addstar@addstar.com.au>
2014-11-13 21:49:08 +1100

    This should fix players being stuck in the arena when relogging

--- src/main/java/org/mcsg/survivalgames/events/JoinEvent.java
+++ src/main/java/org/mcsg/survivalgames/events/JoinEvent.java
@@ -3,6 +3,7 @@ package org.mcsg.survivalgames.events;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.Plugin;
@@ -18,16 +19,22 @@ public class JoinEvent implements Listener {
         this.plugin = plugin;
     }
 
-	@EventHandler
+	@EventHandler(priority=EventPriority.HIGHEST, ignoreCancelled=true)
     public void PlayerJoin(PlayerJoinEvent e) {
         final Player p = e.getPlayer();
-        
-        if(GameManager.getInstance().getBlockGameId(p.getLocation()) != -1){
-            Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable(){
+
+        if (GameManager.getInstance().getBlockGameId(p.getLocation()) != -1) {
+        	// Send to lobby if the player re-logs inside the arena
+        	p.teleport(SettingsManager.getInstance().getLobbySpawn());
+
+        	Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
                 public void run() {
-                    p.teleport(SettingsManager.getInstance().getLobbySpawn());
+                    if ((p.isOnline()) && (GameManager.getInstance().getBlockGameId(p.getLocation()) != -1)) {
+                    	// Safe guard in case previous teleport didn't work (it sometimes leaves players in the arena after relogging)
+                    	p.teleport(SettingsManager.getInstance().getLobbySpawn());
+                    }
                 }
-            }, 5L);
+            }, 15L);
         }
     }
     
