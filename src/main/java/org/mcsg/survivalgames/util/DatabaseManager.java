adbaa8dfba70666e50d93481bf84703ba013940b
Narimm <benjicharlton@gmail.com>
2018-06-13 23:37:29 +1000

    Update to 0.6.2-SNAPSHOT
    Update the way we handle db connection. Support properties which allow Mysql 5.7 support.   Properties are a list of keys and values that match jbdc connection properties ..eg `useSSL`

--- src/main/java/org/mcsg/survivalgames/util/DatabaseManager.java
+++ src/main/java/org/mcsg/survivalgames/util/DatabaseManager.java
@@ -5,9 +5,13 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
 import java.util.logging.Logger;
 
 
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.Plugin;
 import org.mcsg.survivalgames.SettingsManager;
@@ -40,27 +44,28 @@ public class DatabaseManager {
         return conn;
     }
 
-    public boolean connectToDB(String host, int port, String db, String user, String pass)
-    {
-        try
-        {
+    @Deprecated
+    public boolean connectToDB(String host, int port, String db, String user, String pass) {
+        Properties props = new Properties();
+        props.put("user", user);
+        props.put("password", pass);
+        return connectToDB(host, port, db, props);
+    }
+
+    public boolean connectToDB(String host, int port, String db, Properties props) {
+        try {
             Class.forName("com.mysql.jdbc.Driver");
-            conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db, user, pass);
+            conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + db, props);
             return true;
-        }
-        catch (ClassNotFoundException e)
-        {
+        } catch (ClassNotFoundException e) {
             log.warning("Couldn't start MySQL Driver. Stopping...\n" + e.getMessage());
 
             return false;
-        }
-        catch (SQLException e)
-        {
+        } catch (SQLException e) {
             log.warning("Couldn't connect to MySQL database. Stopping...\n" + e.getMessage());
             return false;
         }
     }
-
     public PreparedStatement createStatement(String query)
     {
         int times = 0;
@@ -101,14 +106,21 @@ public class DatabaseManager {
 
     public boolean connect()
     {
-        //log.info("Connecting to database...");
         FileConfiguration c = SettingsManager.getInstance().getConfig();
+
+        Properties props = new Properties();
+        props.put("user", c.getString("sql.user", "root"));
+        props.put("password", c.getString("sql.pass", ""));
         String host =  c.getString("sql.host", "localhost");
         int port    =  c.getInt("sql.port",  3306);
         String db   =  c.getString("sql.database", "SurvivalGames");
-        String user =  c.getString("sql.user", "root");
-        String pass =  c.getString("sql.pass",  "");
-        return this.connectToDB(host, port, db, user,pass);
+        ConfigurationSection dbprops = c.getConfigurationSection("sql.properties");
+        if (dbprops != null) {
+            for (Map.Entry<String, Object> prop : dbprops.getValues(false).entrySet()) {
+                props.put(prop.getKey(), prop.getValue());
+            }
+        }
+        return this.connectToDB(host, port, db, props);
     }
 
 }
